b72610c83f93cc5e560fabb8e6106eafcaa72b4a
AlexanderTomic <mirela.tomic35@gmail.com>
2022-01-19 14:42:23 +0100

    Spielregeln im Alert hinzugefügt mit den Anzahl der Schiffen

--- src/main/java/at/ac/fhcampuswien/battleship/game/BattleShipMain.java
+++ src/main/java/at/ac/fhcampuswien/battleship/game/BattleShipMain.java
@@ -36,31 +36,6 @@ public class BattleShipMain extends Application { //BattleShipMain ist Subklasse
         primaryStage.show(); //Damit sich das Fenster öffnen müssen wir unser PrimaryStage mit "show" aufrufen
 
 
-
-       /* Game game = new Game(primaryStage); //Objekt mit Variablennamen "game" der Klasse Game wird erstellt, mit dem "primaryStage"
-        // --> Ist eine Instanzvariable, wird im Konstrukor aufgerufen, wenn ein Spiel gestartet wird
-                                            //In primaryStage ist das Spielfenster gespeichert
-
-        game.createMyGame(); //In game wird dann die "createMyGame" Methode aus der Klasse "Game" aufgerufen
-        // --> Spieler werden erstellt, wieviel Schiffe man platzieren kann, Erstellung der 10*10 Kästchen usw.
-
-        Scene scene = new Scene(game.getRoot()); //Objekt der Klasse Scene erstellt, welches das den getter "root" aufruft.
-        // im "root" haben wir die Buttons, Texte, genaue Positionierung unserer HBoxen in der Klasse "Game"
-
-        primaryStage.getIcons().add(new Image("file:src/main/resources/image/img_4.png")); //Im primaryStage wird die Funktion getIcons(javafx) aufgerufen,
-        // und ein Foto hinzugefügt, dass unser Spielfeld abdeckt nach setzen der Schiffe
-
-
-        primaryStage.setTitle("Battleship"); //Der Titel des Fensters trägt den Namen "Battleship"
-
-        primaryStage.setScene(scene); // Funktion "setScene" wird aufgerufen -
-        // -> "scene" wird gesetzt durch "setScene" Funktion, das css stylesheet gehört zum Styling der "scene"
-
-        primaryStage.setResizable(false); //Hier haben wir die "setResizable" Funktion aufgerufen die sagt,
-        // dass das Fenster von der Höhe & Breite nicht veränderbar ist --> false
-
-        primaryStage.show(); //Damit sich das Fenster öffnen müssen wir unser PrimaryStage mit "show" aufrufen
-    }*/
     }
 
     private Parent createContent() { //Methode createContent von Parent
@@ -90,8 +65,8 @@ public class BattleShipMain extends Application { //BattleShipMain ist Subklasse
 
                     Alert alert = new Alert(Alert.AlertType.INFORMATION); //Informationsfenster bevor das Spiel los geht
                     alert.setTitle("Battleships basic rules");
-                    alert.setHeaderText(" -ships can be placed next to each others but cant overlap \n -every player has 10 ships ");
-                    alert.setContentText("Place your 10 ships at the board then try to find enemy's");
+                    alert.setHeaderText(" - Ships can be placed next to each others but cant overlap \n - Every player has 10 ships ");
+                    alert.setContentText("Place your 10 ships at the board then try to find enemy's \n - Place 1 Battleship (5 blocks), 2 Cruiser (4 blocks), 3 Destroyer (3 blocks), 4 Submarines (2 blocks)");
                     alert.showAndWait();
                 })),
 
